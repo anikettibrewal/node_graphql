a64f1860fff10e8c390456ef7f5c6480d685014c
Aniket <aniket@Aniket.local>
2021-05-20 11:07:23 +0530

    node + graphql example-1

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const PORT = 6969;
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./Schemas/index");
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+})) 
+
+app.listen(PORT, ()=> {
+    console.log("server running")
+});
